f3e5c7dd3e2e05b08a787fa5cb726de616bb289a
hossamemamo <78453559+hossamemamo@users.noreply.github.com>
2023-04-29 20:52:19 +0300

    tcp header

--- Client.py
+++ Client.py
@@ -1,26 +1,92 @@
 import socket
 import random
 import zlib
+import struct
+
+#TCP header
+# Fields:
+# Source port (unsigned short 2 bytes): this is a 16 bit field that specifies the port number of the sender. 
+# Destination port (unsigned short 2 bytes): this is a 16 bit field that specifies the port number of the receiver.
+# Sequence number (unsigned int 4 bytes): the sequence number is a 32 bit field that indicates how much data is sent during the TCP session. When you establish a new TCP connection (3 way handshake) then the initial sequence number is a random 32 bit value. The receiver will use this sequence number and sends back an acknowledgment. Protocol analyzers like wireshark will often use a relative sequence number of 0 since it’s easier to read than some high random number.
+# Acknowledgment number (unsigned int 4 bytes): this 32 bit field is used by the receiver to request the next TCP segment. This value will be the sequence number incremented by 1.
+# DO(2 bytes1): this is the 4 bit data offset field, also known as the header length. It indicates the length of the TCP header so that we know where the actual data begins.
+# RSV(2 bytes2): these are 3 bits for the reserved field. They are unused and are always set to 0.
+# Flags(2 bytes3 ->unsigned short): there are 9 bits for flags, we also call them control bits. We use them to establish connections, send data and terminate connections:
+    # URG  : urgent pointer. When this bit is set, the data should be treated as priority over other data.
+    # ACK: used for the acknowledgment.
+    # PSH: this is the push function. This tells an application that the data should be transmitted immediately and that we don’t want to wait to fill the entire TCP segment.
+    # RST: this resets the connection, when you receive this you have to terminate the connection right away. This is only used when there are unrecoverable errors and it’s not a normal way to finish the TCP connection.
+    # SYN: we use this for the initial three way handshake and it’s used to set the initial sequence number.
+    # FIN: this finish bit is used to end the TCP connection. TCP is full duplex so both parties will have to use the FIN bit to end the connection. This is the normal method how we end an connection.
+# Window(unsigned short): the 16 bit window field specifies how many bytes the receiver is willing to receive. It is used so the receiver can tell the sender that it would like to receive more data than what it is currently receiving. It does so by specifying the number of bytes beyond the sequence number in the acknowledgment field.
+# Checksum(unsigned short): 16 bits are used for a checksum to check if the TCP header is OK or not.
+# Urgent pointer(unsigned short): these 16 bits are used when the URG bit has been set, the urgent pointer is used to indicate where the urgent data ends.
+# Options(not implemented): this field is optional and can be anywhere between 0 and 320 bits.
+
+
+
+
+
+
+
+
+
 data = []
 data.append("Saeed")
 data.append("Hossam")
 data.append("Ehab")
+
+clientAddressPort=("127.0.0.1", 20023)
 serverAddressPort = ("127.0.0.1", 20001)
 
-bufferSize = 1024
 
 # Create a UDP socket at client side
 
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPClientSocket.bind(clientAddressPort)
+
 seq_num = 0
+
+
+
+
+
 while seq_num < len(data):
     checksum = zlib.crc32(data[seq_num].encode())
     packet = str(seq_num) + '|'+ str(checksum) + '|' + data[seq_num]
-    if random.random() > 0.1:
-        UDPClientSocket.sendto(packet.encode(), serverAddressPort)
+    if random.random() > 0:
+
+        Source_port=("127.0.0.1", 20023)
+        Destination_port=("127.0.0.1", 20001)
+
+        Source_port_t=b'127.0.0.1:20023'
+        Destination_port_t=b'127.0.0.1:20001'
+
+        Sequence_number=0
+        data="hossam"
+        Acknowledgment_number=0+len(data)
+
+        Flags=b'0101000000000000'
+        
+        bufferSize = 1024
+        checksum = zlib.crc32(data.encode())
+
+        Window=bufferSize
+        Check_sum=checksum
+        Urgent_pointer=0
+
+        
+        TCP_Header=struct.pack(f"2s 2s 2I 4H {len(data)}s",Source_port_t,Destination_port_t
+                            ,Sequence_number,Acknowledgment_number,
+                            int.from_bytes(Flags, byteorder='big'),Window,Check_sum,Urgent_pointer,data)
+
+
+
+        UDPClientSocket.sendto(TCP_Header, serverAddressPort)
         print("Packet sent")
     else:
         print("Packet lost")
+        
     print('Sent packet:', seq_num)
     UDPClientSocket.settimeout(1.0)
     try:
--- Server.py
+++ Server.py
@@ -2,9 +2,11 @@ import socket
 import time
 import hashlib
 import zlib
-localIP = "127.0.0.1"
+import struct
 
-localPort = 20001
+clientAddressPort=("127.0.0.1", 20023)
+serverAddressPort = ("127.0.0.1", 20001)
+
 
 bufferSize = 1024
 
@@ -15,25 +17,47 @@ bytesToSend = str.encode(msgFromServer)
 # Create a datagram socket
 
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPServerSocket.bind((localIP, localPort))
+UDPServerSocket.bind(serverAddressPort)
+
 expected_seq_num = 0
 print("UDP server up and listening")
 # Listen for incoming datagrams
 while (True):
-    data,addr = UDPServerSocket.recvfrom(bufferSize)
-    packet = data.decode().split('|')
-    seq_num = int(packet[0])
-    checksum = packet[1]
-    original_checksum = zlib.crc32(packet[2].encode())
+    #data,addr = UDPServerSocket.recvfrom(bufferSize)
+    data = UDPServerSocket.recvfrom(bufferSize)
+    size_of_payload=len(data)-20
+    unpacked_data = struct.unpack(f"2H 2I 4H {size_of_payload}s", data)
+
+    source_port = unpacked_data[0]
+    destination_port = unpacked_data[1]
+    sequence_number = unpacked_data[2]
+    acknowledgment_number = unpacked_data[3]
+    flags = unpacked_data[4]
+    window = unpacked_data[5]
+    check_sum = unpacked_data[6]
+    urgent_pointer = unpacked_data[7]
+    payload = unpacked_data[8]
+
+
+
+
+
+
+
+    packet = payload
+    seq_num = sequence_number
+    checksum = check_sum
+    original_checksum = zlib.crc32(check_sum.encode())
+
+
     if int(checksum)==original_checksum:
         if seq_num == expected_seq_num:
             ack = str(seq_num)
-            UDPServerSocket.sendto(ack.encode(), addr)
-            print('Received packet:', packet[2].encode("utf-8"))
+            UDPServerSocket.sendto(ack.encode(), clientAddressPort)
+            print('Received packet:', packet.encode("utf-8"))
             expected_seq_num += 1
         else:
             print('Received duplicate packet:', seq_num)
     else:
         print("Message corrupted,waiting for a retransmission")
     time.sleep(1.2)
-    
