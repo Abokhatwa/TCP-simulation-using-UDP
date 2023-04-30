2d48a9adb9d145a089410d13cddf4f856216f2e4
Saeed <samosavo40@gmail.com>
2023-04-30 19:02:15 +0300

    Added 3-way-Handshaking

--- utilities.py
+++ utilities.py
@@ -91,12 +91,12 @@ def Tcp_send(data,Sequence_number,Acknowledgment_number,Flags,Window,Urgent_poin
 
     if random.random() > 0:
       UDPClientSocket.sendto(TCP_header+data, dest_addr)
-      print("Packet sent")
+      #print("Packet sent")
     else:
         print("Packet lost")
 
 
-    print('Sent packet:', Sequence_number)
+    #print('Sent packet:', Sequence_number)
 
 
     
@@ -107,12 +107,13 @@ def tcp_recv(window_size,UDPServerSocket):
 
     print("UDP server up and listening")
     # Listen for incoming datagrams
-
+    serverSeqnumber = 0
     expected_seq_num = 0
-
+    three_way_flag=0
+    print("Waiting for connection.....")
     while True:
         data, src_addr = UDPServerSocket.recvfrom(window_size)
-
+        
         size_of_payload=len(data)-20
         unpacked_data=struct.unpack(f"2H 2I 4H {size_of_payload}s",data)
 
@@ -125,33 +126,47 @@ def tcp_recv(window_size,UDPServerSocket):
         check_sum = unpacked_data[6]
         urgent_pointer = unpacked_data[7]
         payload = unpacked_data[8]
+        if three_way_flag==0:
+            if flags == 20482:
+                print("SYN received, Sending a SYN-ACK")
+                serverFlags = 20498
+                Tcp_send("",serverSeqnumber,sequence_number+1,serverFlags
+                ,window,urgent_pointer,UDPServerSocket,src_addr,src_addr=UDPServerSocket.getsockname())
+                data, src_addr = UDPServerSocket.recvfrom(window_size)
+                size_of_payload=len(data)-20
+                unpacked_data=struct.unpack(f"2H 2I 4H {size_of_payload}s",data)
+                flags = unpacked_data[4]
+                if flags == 20496:
+                    print("Connection established!")
+                    three_way_flag=1
+                    print({"---------------------------------------------------------------"})
 
-
-
-        src_ip=ip2int(src_addr[0])
-        dest_ip=ip2int(UDPServerSocket.getsockname()[0])
-        
-        src_ip = struct.pack('!4B', *src_ip)
-        dest_ip = struct.pack('!4B', *dest_ip)
-
-
-        pseudo_header = struct.pack('!BBH', Reserved, protocol, len(data))
-        pseudo_header = src_ip + dest_ip + pseudo_header
-
-        TCP_header=struct.pack(f"2H 2I 4H",source_port,destination_port
-                ,sequence_number,acknowledgment_number,
-                flags,window,0,urgent_pointer)
-        
-        verify = verify_checksum(pseudo_header + TCP_header + payload, check_sum)
-        if verify == 0xFFFF:
-            if sequence_number == expected_seq_num:
-                ack = str(sequence_number)
-                clientAddressPort=src_addr
-                UDPServerSocket.sendto(ack.encode(), clientAddressPort)
-                print('Received packet:', payload)
-                expected_seq_num += 1
-            else:
-                print('Received duplicate packet:', sequence_number)
         else:
-            print("Message corrupted,waiting for a retransmission")
-        time.sleep(1.2)
+            src_ip=ip2int(src_addr[0])
+            dest_ip=ip2int(UDPServerSocket.getsockname()[0])
+            
+            src_ip = struct.pack('!4B', *src_ip)
+            dest_ip = struct.pack('!4B', *dest_ip)
+
+
+            pseudo_header = struct.pack('!BBH', Reserved, protocol, len(data))
+            pseudo_header = src_ip + dest_ip + pseudo_header
+
+            TCP_header=struct.pack(f"2H 2I 4H",source_port,destination_port
+                    ,sequence_number,acknowledgment_number,
+                    flags,window,0,urgent_pointer)
+            
+            verify = verify_checksum(pseudo_header + TCP_header + payload, check_sum)
+            if verify == 0xFFFF:
+                if sequence_number == expected_seq_num:
+                    ack = str(sequence_number)
+                    clientAddressPort=src_addr
+                    UDPServerSocket.sendto(ack.encode(), clientAddressPort)
+                    print('Received packet:', payload)
+                    expected_seq_num += 1
+                else:
+                    print('Received duplicate packet:', sequence_number)
+            else:
+                print("Message corrupted,waiting for a retransmission")
+            if random.random() < 0.3:
+                time.sleep(1.2)
